7f70bb756a5dcf0628d82068f7d19457a764b8a5
Marius G <mariusgrimstad@yahoo.se>
2018-06-19 13:09:51 +0200

    added more tests

--- test_move.c
+++ test_move.c
@@ -2,18 +2,39 @@
 #include "move.h"
 #include "board.h"
 
+#define TEST_MOVE(var,eq,piece) cr_assert_eq(var, eq, "%s : number of moves %d == %d", piece, var, eq);
+
 Test(simple, test){
   int moves[BOARD_SIZE*BOARD_SIZE];
 
+  /* EMPTY BOARD */
+
+  // pawn
   int numMoves = possibleMoves(BLACK_PAWN, POS(0,0),moves);
-  cr_assert_eq(numMoves, 1, "number of moves %d == %d", numMoves, 1);
+  TEST_MOVE(numMoves,1,"pawn")
+  // ------
 
-  numMoves = possibleMoves(BLACK_BISHOP, POS(3,3),moves);
-  /** cr_assert_eq(numMoves, 2, "number of moves %d == %d", numMoves, 2); */
+  // knight
+  numMoves = possibleMoves(BLACK_KNIGHT, POS(0,6),moves);
+  TEST_MOVE(numMoves,3,"knight")
 
+  numMoves = possibleMoves(BLACK_KNIGHT, POS(0,7),moves);
+  TEST_MOVE(numMoves,2,"knight")
+  // ------
 
+  // bishop
+  numMoves = possibleMoves(BLACK_BISHOP, POS(0,5),moves);
+  TEST_MOVE(numMoves,7,"bishop")
+  // ------
 
-  /** for(int i = 0; i < numMoves; i++){ */
-  /**   printf("%d,%d\n", ROW(moves[i]),COL(moves[i])); */
-  /** } */
+  // rook
+  numMoves = possibleMoves(BLACK_ROOK, POS(0,7),moves);
+  TEST_MOVE(numMoves,14,"rook")
+  // ------
+
+  // queen
+  numMoves = possibleMoves(BLACK_QUEEN, POS(0,3),moves);
+  TEST_MOVE(numMoves,21,"rook")
+  // ------
+  /* ############ */
 }
