c6961768aede44257a65aa570a2003c55edbc397
Marius G <mariusgrimstad@yahoo.se>
2018-06-20 11:37:41 +0200

    corrected tests

--- test_move.c
+++ test_move.c
@@ -65,17 +65,17 @@ Test(moves, test_initial_board){
 
   // bishop
   numMoves = possibleMoves(&b, BLACK_BISHOP, POS(0,5),moves);
-  TEST_MOVE(numMoves,7,"bishop")
+  TEST_MOVE(numMoves,0,"bishop")
   // ------
 
   // rook
   numMoves = possibleMoves(&b, BLACK_ROOK, POS(0,7),moves);
-  TEST_MOVE(numMoves,14,"rook")
+  TEST_MOVE(numMoves,0,"rook")
   // ------
 
   // queen
   numMoves = possibleMoves(&b, BLACK_QUEEN, POS(0,3),moves);
-  TEST_MOVE(numMoves,21,"rook")
+  TEST_MOVE(numMoves,0,"queen")
   // ------
   /* ############ */
 }
