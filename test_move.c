b508001c070f737704200ebcfde509fd7861946d
Marius G <mariusgrimstad@yahoo.se>
2018-06-20 14:29:07 +0200

    new tests

--- test_move.c
+++ test_move.c
@@ -8,6 +8,7 @@ int moves[BOARD_SIZE*BOARD_SIZE];
 Board b;
 
 void setup(void) {
+    printf("clearing board\n");
     board_clear(&b);
 }
 
@@ -78,4 +79,40 @@ Test(moves, test_initial_board){
   TEST_MOVE(numMoves,0,"queen")
   // ------
   /* ############ */
+}
+
+Test(moves, test_bishop_corner_board_same_color){
+
+  board_set(&b, POS(0,0), WHITE_BISHOP);
+  board_set(&b, POS(6,6), WHITE_BISHOP);
+  board_set(&b, POS(7,7), WHITE_BISHOP);
+
+  int numMoves = possibleMoves(&b, WHITE_BISHOP, POS(0,0),moves);
+  TEST_MOVE(numMoves,5,"bishop no attack")
+}
+
+Test(moves, test_bishop_corner_board_different_color){
+
+  board_set(&b, POS(0,0), WHITE_BISHOP);
+  board_set(&b, POS(6,6), BLACK_BISHOP);
+  board_set(&b, POS(7,7), BLACK_BISHOP);
+
+  int numMoves = possibleMoves(&b, WHITE_BISHOP, POS(0,0),moves);
+  TEST_MOVE(numMoves,6,"bishop can attack")
+}
+
+Test(moves, test_queen_middle){
+
+  board_set(&b, POS(3,3), WHITE_QUEEN);
+  board_set(&b, POS(1,3), WHITE_KING);
+  board_set(&b, POS(3,1), WHITE_KING);
+  board_set(&b, POS(6,3), WHITE_KING);
+  board_set(&b, POS(3,6), WHITE_KING);
+  board_set(&b, POS(0,0), BLACK_ROOK);
+  board_set(&b, POS(0,6), BLACK_ROOK);
+  board_set(&b, POS(6,0), BLACK_ROOK);
+  board_set(&b, POS(7,7), BLACK_ROOK);
+
+  int numMoves = possibleMoves(&b, WHITE_QUEEN, POS(3,3),moves);
+  TEST_MOVE(numMoves,19,"queen partly attacking")
 }
